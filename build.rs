e03675092a50a183a214632805a2c1e6455c58f2
michael1011 <me@michael1011.at>
2024-10-08 14:04:52 +0200

    chore: update dependencies

--- build.rs
+++ build.rs
@@ -1,8 +1,9 @@
 fn main() {
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
-        .compile(&["protos/hold.proto"], &["protos"])
+        .compile_protos(&["protos/hold.proto"], &["protos"])
         .unwrap_or_else(|e| panic!("Could not build protos: {}", e));
+
     built::write_built_file()
         .unwrap_or_else(|e| panic!("Failed to acquire build-time information: {}", e));
 }
